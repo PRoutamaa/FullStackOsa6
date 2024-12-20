1e413415a578a99253c0dcadd6b21f74cd761ec7
PRoutamaa <89683328+PRoutamaa@users.noreply.github.com>
2024-12-20 11:35:56 +0200

    Tasks 6.1.-6.8. done

---
+++ redux-anecdotes/src/App.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
-import App from './App'
+import { createStore } from 'redux'
 import counterReducer from './reducers/reducer'
 
 const store = createStore(counterReducer)
@@ -46,11 +44,7 @@ const App = () => {
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const renderApp = () => {
   root.render(<App />)
